3ef36d930cb6845ff51d65af1e2aece9b63fe646
sankalp04 <sankalp_patro@hotmail.com>
2019-07-08 00:32:44 -0700

    Add gravity as reset parameter; set in ReacherAcademy

--- UnitySDK/Assets/ML-Agents/Examples/Reacher/Scripts/ReacherAcademy.cs
+++ UnitySDK/Assets/ML-Agents/Examples/Reacher/Scripts/ReacherAcademy.cs
@@ -13,6 +13,7 @@ public class ReacherAcademy : Academy {
     {
         goalSize = (float)resetParameters["goal_size"];
         goalSpeed = (float)resetParameters["goal_speed"];
+        Physics.gravity = new Vector3(0, -resetParameters["gravity"], 0);
     }
 
     public override void AcademyStep()
